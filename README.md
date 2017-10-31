ebc94ef816bb30907d273d8c5dc36579a614d141
Alex Goley <ajgoley@gmail.com>
2017-10-30 23:08:18 -0400

    name changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# spark-animate
-Spark Animate is a simple and light weight JavaScript library for animations. Compatible with most browsers.
+# pallet-animate
+Pallet Animate is a simple and light weight JavaScript library for animations. Compatible with most browsers.
--- pallet-animate.js
+++ pallet-animate.js
@@ -1,6 +1,6 @@
 /**
- * SPARK skanimate JS
- * Spark skanimate s a simple and light weight JavaScript library for animations. Compatible with most browsers.
+ * PALLET ANIMATE JS
+ * Pallete Animate s a simple and light weight JavaScript library for animations. Compatible with most browsers.
  */
 
 // Check Browser Version
@@ -30,8 +30,8 @@ browserChecks.isChrome=!!window.chrome && !!window.chrome.webstore;
 browserChecks.isBlink= (browserChecks.isChrome || browserChecks.isOpera) && !!window.CSS;  
 
 // VARIABLES
-var skanimate = {}; // initialize the skanimate object.
-skanimate.directions = ['up', 'down', 'left', 'right'];
+var ptanimate = {}; // initialize the ptanimate object.
+ptanimate.directions = ['up', 'down', 'left', 'right'];
 
 
 // FUNCTIONS
@@ -48,12 +48,12 @@ skanimate.directions = ['up', 'down', 'left', 'right'];
  * @param {number} index - optional: If a non unique identifier is passed use this as the index to use. Defualts to 0. 
  * @return {void}
  */
-skanimate.move = function (identifier, direction, callback, step, distance, index) {    
-    var el = (identifier instanceof HTMLElement)? identifier : skanimate.getElementByIdentifier(identifier, index);
+ptanimate.move = function (identifier, direction, callback, step, distance, index) {    
+    var el = (identifier instanceof HTMLElement)? identifier : ptanimate.getElementByIdentifier(identifier, index);
     if (!el) return;
 
-    if (skanimate.directions.indexOf(direction) < 0) {
-        console.error('spark skanimate: unrecognized direction %s', direction);
+    if (ptanimate.directions.indexOf(direction) < 0) {
+        console.error('spark ptanimate: unrecognized direction %s', direction);
         return;
     }
 
@@ -62,7 +62,7 @@ skanimate.move = function (identifier, direction, callback, step, distance, inde
     var dist;
     if (distance) {
         if ((typeof distance) != 'number') {
-            console.error('skark skanimate: distance must be of type number');
+            console.error('skark ptanimate: distance must be of type number');
         }
         dist = distance;
     } else {
@@ -74,7 +74,7 @@ skanimate.move = function (identifier, direction, callback, step, distance, inde
         if (direction === 'right') dist += el.parentElement.offsetWidth;
     }
 
-    var pos = ['up', 'down'].indexOf(direction) >= 0 ? skanimate.getElementPos(el, 'top') : skanimate.getElementPos(el, 'left');
+    var pos = ['up', 'down'].indexOf(direction) >= 0 ? ptanimate.getElementPos(el, 'top') : ptanimate.getElementPos(el, 'left');
     var id = setInterval(frame, 1);
     function frame() {
         if (dist == 0) {
@@ -90,7 +90,7 @@ skanimate.move = function (identifier, direction, callback, step, distance, inde
     }
 }
 
-skanimate.getElementPos = function (el, edge) {
+ptanimate.getElementPos = function (el, edge) {
     if (edge === 'top') return p = el.style.top? parseInt(el.style.top.substring(0, el.style.top.length - 2)) : 0;
     if (edge === 'left') return el.style.left? parseInt(el.style.left.substring(0, el.style.left.length - 2)) : 0;
 }
@@ -100,11 +100,11 @@ skanimate.getElementPos = function (el, edge) {
  * @param {string} identifier - html id, class name, tag name, or name.
  * @return {HTMLElement}
  */
-skanimate.getElementByIdentifier = function (identifier, index) {
+ptanimate.getElementByIdentifier = function (identifier, index) {
     var e = document.getElementById(identifier);
     if (!e) e = document.getElementsByClassName(identifier)[(index || 0)];
     if (!e) e = document.getElementsByTagName(identifier)[(index || 0)];
-    if (!e) console.error('skark skanimate: could not find an element with identifier %s and index %d', identifier, index);
+    if (!e) console.error('skark ptanimate: could not find an element with identifier %s and index %d', identifier, index);
     return e;
 }
 
@@ -116,11 +116,11 @@ skanimate.getElementByIdentifier = function (identifier, index) {
  * @param {boolean} mobileDisable - if true will not call function is screen size is of mobile width
  */
 
-skanimate.skScroll = function(threshold, identifier, classname, mobileDisable){
+ptanimate.scroll = function(threshold, identifier, classname, mobileDisable){
     // Check if mobile width
     if(mobileDisable && window.innerWidth <= 640) return;
         
-    var el = skanimate.getElementByIdentifier(identifier, 0);
+    var el = ptanimate.getElementByIdentifier(identifier, 0);
     var scrollHeight = (browserChecks.isIE ? window.pageYOffset : window.scrollY);
         
     if (!el) return;
